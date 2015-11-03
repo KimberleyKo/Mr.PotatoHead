a4959ae0f50ba56bcf5e9f9ff11d16ff9d4157cd
Kimberley <kimberleydaniela@gmail.com>
2015-11-03 21:26:41 +0100

    definitive version of second app

--- app/src/main/java/com/example/kimberley/mrpotatoheadsecondapp/MrPotatoHead_MainActivity.java
+++ app/src/main/java/com/example/kimberley/mrpotatoheadsecondapp/MrPotatoHead_MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.kimberley.mrpotatoheadsecondapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+import static com.example.kimberley.mrpotatoheadsecondapp.R.*;
 
 public class MrPotatoHead_MainActivity extends AppCompatActivity {
+    ImageView Ears;
+    ImageView Shoes;
+    ImageView Hat;
+    ImageView Arms;
+    ImageView Eyes;
+    ImageView Eyebrows;
+    ImageView Glasses;
+    ImageView Mouth;
+    ImageView Mustache;
+    ImageView Nose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mr_potato_head__main);
+        setContentView(layout.activity_mr_potato_head__main);
+
     }
-}
+
+    public void checkAll(View view) {
+        switch (view.getId()) {
+            case id.checkboxEars :
+                Ears = (ImageView) findViewById(id.Ears);
+                Ears.setVisibility(Ears.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxArms :
+                Arms = (ImageView) findViewById(id.Arms);
+                Arms.setVisibility(Arms.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxShoes :
+                Shoes = (ImageView) findViewById(id.Shoes);
+                Shoes.setVisibility(Shoes.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxEyebrows :
+                Eyebrows = (ImageView) findViewById(id.Eyebrows);
+                Eyebrows.setVisibility(Eyebrows.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxEyes :
+                Eyes = (ImageView) findViewById(id.Eyes);
+                Eyes.setVisibility(Eyes.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxGlasses :
+                Glasses = (ImageView) findViewById(id.Glasses);
+                Glasses.setVisibility(Glasses.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxMouth :
+                Mouth = (ImageView) findViewById(id.Mouth);
+                Mouth.setVisibility(Mouth.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxNose :
+                Nose = (ImageView) findViewById(id.Nose);
+                Nose.setVisibility(Nose.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxMustache :
+                Mustache = (ImageView) findViewById(id.Mustache);
+                Mustache.setVisibility(Mustache.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+            case id.checkboxHat :
+                Hat = (ImageView) findViewById(id.Hat);
+                Hat.setVisibility(Hat.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                break;
+        }
+    }
+    /*
+    public void checkEars(View view){
+        Ears = (ImageView) findViewById(id.Ears);
+        Ears.setVisibility(View.VISIBLE);
+    }
+
+    public void checkShoes(View view){
+        Shoes = (ImageView) findViewById(id.Shoes);
+        Shoes.setVisibility(Shoes.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+    }
+
+    public void checkHat(View view){
+        Hat = (ImageView) findViewById(id.Hat);
+        Hat.setVisibility(View.VISIBLE);
+    }*/
+
+    }
+
+
