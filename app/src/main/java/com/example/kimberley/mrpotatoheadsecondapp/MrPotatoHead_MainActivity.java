5d238aba234b1ac18bf0f4257e8747c424448965
Kimberley <kimberleydaniela@gmail.com>
2015-11-03 12:32:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kimberley/mrpotatoheadsecondapp/MrPotatoHead_MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kimberley.mrpotatoheadsecondapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MrPotatoHead_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mr_potato_head__main);
+    }
+}
